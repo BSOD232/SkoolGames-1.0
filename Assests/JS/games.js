5cf47cfd438631fd18151d2b1b80b80f2c6b406e
platformerdotio <smartfoloo@gmail.com>
2023-12-13 21:17:34 -0600

    gg

--- Assests/JS/games.js
+++ Assests/JS/games.js
@@ -1,3 +1,25 @@
 function openGame(game) {
     window.location.href = game + ".html"
 }
+
+const gameList = document.getElementById("Games-List");
+const searchInput = document.getElementById("GameSearchBar");
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  const gameCards = gameList.querySelectorAll(".Games-Game");
+
+  gameCards.forEach((card) => {
+    const title = card.querySelector(".title").textContent.toLowerCase();
+    if (title.includes(searchTerm)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  gameList.style.removeProperty("display");
+  gameList.style.removeProperty("flex-wrap");
+
+  const visibleCards = Array.from(gameList.querySelectorAll(".game-card[style='display: block;']"));
+});
